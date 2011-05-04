e7da9ac667b5998417f5984cb30f56521385bb81
Allen Li <cyberdupo56@gmail.com>
2011-05-04 08:45:45 -0700

    checking with python2 so I can use Twisted

--- mahjong/version.py
+++ mahjong/version.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # mahjong
 # GPLv3 2010 Allen Li
 
 version = (0, 2)
-python_version = (3, 1, 2)
+python_version = (2, 7, 1)
 pygame_version = (1, 9, 1)
